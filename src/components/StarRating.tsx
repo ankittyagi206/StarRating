0a35948068768634563d0124dca0bd63e61b47d5
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-06-27 07:50:18 +0530

    feat: Star rating component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,5 +1,6 @@
 import { AiFillStar } from "react-icons/ai";
 import { useState } from "react";
+import React from "react";
 
 const StartRating = () => {
   const [rating, setRating] = useState(0);
