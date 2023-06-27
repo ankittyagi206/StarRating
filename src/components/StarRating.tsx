6accf811102ee83f1063eeb783215829eb9a7a89
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-06-27 07:49:41 +0530

    first commit

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,44 @@
+import { AiFillStar } from "react-icons/ai";
+import { useState } from "react";
+
+const StartRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  console.log(rating);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      {[...Array(5)].map((star, index) => {
+        const starRating = index + 1;
+        return (
+          <div
+            key={index}
+            style={{ cursor: "pointer" }}
+            onClick={() => setRating(starRating)}
+            onMouseEnter={() => setHover(starRating)}
+            onMouseLeave={() => setHover(0)}
+          >
+            <input
+              style={{ display: "none" }}
+              type="radio"
+              value={starRating}
+            />
+            <AiFillStar
+              size="100px"
+              color={starRating <= (hover || rating) ? "#D5AB55" : "black"}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StartRating;
